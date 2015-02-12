2086851402a6f0dff375ffbec538505900bce0d6
mjkaufer <mjkaufer@gmail.com>
2015-02-11 20:32:23 -0500

    Can create (poor) field lines, track potential charge

--- js/script.js
+++ js/script.js
@@ -13,6 +13,13 @@ var negativeColor = 0xff0000;
 var positiveColor = 0x0000ff;
 var testCharge;
 var testChargeArrow;
+var lineGeo = new THREE.Geometry();
+var lineMaterial = new THREE.LineBasicMaterial({
+	color: 0xff00ff
+});
+var line;
+var negCharge, posCharge;
+var lines = new THREE.Object3D();
 //declare other vars here
 
 init();
@@ -48,6 +55,8 @@ function netChargeVector(testCharge){
 	
 	// testCharge.direction = netCharge;
 
+	console.log("Net potential",netElectricPotential(testCharge))
+
 	return netCharge;
 
 }
@@ -61,6 +70,90 @@ function chargeVector(testCharge, charge){
 	var vector = displacement.normalize().multiplyScalar(magnitude);
 	// console.log(vector)
 	return vector;
+
+}
+
+function electricPotential(testCharge, charge){
+	//formula = q(charge) / r  - we don't care about the k constant because it's a constant and irrelevant here
+	var displacement = testCharge.position.clone().sub(charge.position).length();
+
+	var magnitude = charge.charge;
+	
+	return magnitude / displacement;
+
+}
+
+function netElectricPotential(testCharge){
+
+	//the charges which testCharge is interacting with are in electrons
+	var charges = electrons.children;
+
+	var netPotential = 0;
+
+	for(var i = 0; i < charges.length; i++)
+		netPotential += electricPotential(testCharge, charges[i])
+	
+	// testCharge.direction = netCharge;
+
+	return netPotential;
+
+}
+
+function drawLines(){
+	lines.children = [];
+	for(var i = 0; i < electrons.children.length; i++){
+		var electron = electrons.children[i];
+
+		if(electron.charge <= 0){//its charge is negative
+			continue;
+		}
+
+		console.log(electron)
+
+		var color = new THREE.Color().setHSL(Math.random(), 0.5, 0.5);//specific color for this object
+
+		var elLine = mapLine(electron, new THREE.Vector3(0.1, 0, 0), 3, color);
+
+
+		lines.add(elLine);
+
+	}
+}
+
+function mapLine(electron, offset, fineness, col){
+	
+	if(!fineness)
+		fineness = 3;
+	if(!col)
+		col = new THREE.Color().setHSL(Math.random(), 0.5, 0.5);
+	if(!offset)
+		offset = new THREE.Vector3(0.1,0,0);
+
+
+	var phonyParticle = {
+		position: electron.position.clone().add(offset),
+		charge: 1
+	};
+
+	lineGeo.children = [];
+
+	for(var i = 0; i < 9 * fineness; i++){
+		lineGeo.vertices.push(phonyParticle.position.clone())
+
+		var dir = netChargeVector(phonyParticle);
+
+		dir = dir.normalize().divideScalar(fineness);
+
+		phonyParticle.position = dir.clone().add(phonyParticle.position.clone());
+	}
+
+	lineGeo.vertices.push(phonyParticle.position.clone())
+	var newLineMaterial = lineMaterial.clone();
+	newLineMaterial.color = col;
+
+	line = new THREE.Line(lineGeo, newLineMaterial);
+
+	return line;
 
 }
 
@@ -131,11 +224,12 @@ function init(){
 	scene.add( light );
 	
 	
-    testCharge = createElectron(1, new THREE.Vector3(), testColor);
+    testCharge = createElectron(1, new THREE.Vector3(-3, 2, 0), testColor);
     testChargeArrow = arrowHelper(testCharge.position, new THREE.Vector3());
 
-    var negCharge = createElectron(-1, new THREE.Vector3(5, 3, 0), negativeColor);
-    var posCharge = createElectron(1, new THREE.Vector3(4, -4, 0), positiveColor);
+    negCharge = createElectron(-3, new THREE.Vector3(5, 0, 0), negativeColor);
+    posCharge = createElectron(2, new THREE.Vector3(-4, 2, 0), positiveColor);
+    var posCharge2 = createElectron(2, new THREE.Vector3(4, 2, 0), positiveColor);
 
     
 
@@ -144,7 +238,9 @@ function init(){
 
     electrons.add(negCharge);
     electrons.add(posCharge);
+    electrons.add(posCharge2);
     scene.add(electrons);
+    scene.add(lines);
 
     renderer.setClearColor(0xededed);
     
@@ -181,10 +277,12 @@ function render(){
 	t+=0.01;
 	
 	electricForce = netChargeVector(testCharge);//the direction in which the testCharge will move
-	testCharge.position.add(electricForce)
+	testCharge.position.add(electricForce.clone());
+
 
 	// testChargeArrow.position.sub(testChargeArrow.position).testCharge.position.add(electricForce);//might need to divide by a scalar so it doesn't move too quickly
 	var arrowDirection = electricForce.clone();
+
 	var newMagnitude = Math.max(Math.min(Math.log(arrowDirection.length()), 4), 1);//set the magnitude to the log of its original - ensure it's within the range [1,4]
 
 	testChargeArrow.setDirection(arrowDirection.normalize().multiplyScalar(newMagnitude));
